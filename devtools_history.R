9fb843e1f02344e19a9c7643025b90b24d6ac4fc
ccombesGG4 <82885244+ccombesGG4@users.noreply.github.com>
2021-04-20 15:16:58 +0200

    Add files via upload

---
+++ devtools_history.R
@@ -0,0 +1,9 @@
+# Start documentation
+usethis::use_vignette("how to use the package")
+
+# Add the following packages
+usethis::use_package("stats")
+usethis::use_package("graphics")
+
+# Ignore this file
+usethis::use_build_ignore("devtools_history.R")
